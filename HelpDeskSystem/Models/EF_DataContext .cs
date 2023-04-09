46ceb795fbd47e55825542fc0ac00278e0966d2d
LAPTOP-5QT8CTV3\quang <quanghm2010@gmail.com>
2023-04-09 23:19:03 +0700

    new project

---
+++ HelpDeskSystem/Models/EF_DataContext .cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace HelpDeskSystem.Models
+{
+    public class EF_DataContext : DbContext
+    {
+        public EF_DataContext(DbContextOptions<EF_DataContext> options) : base(options) { }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.UseSerialColumns();
+        }
+        public DbSet<Product> Products
+        {
+            get;
+            set;
+        }
+        public DbSet<Order> Orders
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ HelpDeskSystem/Models/Order.cs
@@ -0,0 +1,45 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HelpDeskSystem.Models
+{
+    public class Order
+    {
+        [Key, Required]
+        public int id
+        {
+            get;
+            set;
+        }
+        public int product_id
+        {
+            get;
+            set;
+        }
+        [Required]
+        public string? name
+        {
+            get;
+            set;
+        }
+        public string? address
+        {
+            get;
+            set;
+        }
+        public string? phone
+        {
+            get;
+            set;
+        }
+        public DateTime createdon
+        {
+            get;
+            set;
+        }
+        public virtual Product product
+        {
+            get;
+            set;
+        }
+    }
+}

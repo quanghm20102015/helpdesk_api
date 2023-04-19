340d56814f543ff0c335f7aaa718acba3ce5f214
LAPTOP-5QT8CTV3\quang <quanghm2010@gmail.com>
2023-04-19 09:29:19 +0700

    edit connectionstring

---
+++ HelpDeskSystem/Migrations/20230415025753_My3Mirgration.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HelpDeskSystem.Migrations
+{
+    /// <inheritdoc />
+    public partial class My3Mirgration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "test1",
+                table: "Accounts",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "test1",
+                table: "Accounts");
+        }
+    }
+}

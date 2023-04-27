ec16b150039f399c3d79823cff3ddef13649fd4d
LAPTOP-5QT8CTV3\quang <quanghm2010@gmail.com>
2023-04-27 12:00:15 +0700

    Merge branch 'develop' of https://github.com/quanghm20102015/helpdesk_api into develop
    # Conflicts:
    #	.vs/HelpDeskSystem/v17/.suo

--- HelpDeskSystem/obj/Debug/net6.0/HelpDeskSystem.AssemblyInfo.cs
+++ HelpDeskSystem/obj/Debug/net6.0/HelpDeskSystem.AssemblyInfo.cs
@@ -11,6 +11,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("4a15494e-4884-48ee-8e72-c84228736756")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelpDeskSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]

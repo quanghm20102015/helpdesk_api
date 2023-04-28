0b65d83981773cbfe2af932f7a8d1b3dc881ed41
LAPTOP-5QT8CTV3\quang <quanghm2010@gmail.com>
2023-04-28 22:10:36 +0700

    Add team, teamAgent

---
+++ HelpDeskSystem/Models/Team.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HelpDeskSystem.Models
+{
+    public class Team
+    {
+        [Key, Required]
+        public int id
+        {
+            get;
+            set;
+        }
+        [Required]
+        public string? name
+        {
+            get;
+            set;
+        }
+        public int idCompany
+        {
+            get;
+            set;
+        }
+    }
+}

1cdf40c4fdbf97615c33bd1b22c849ce7d860f6c
LAPTOP-5QT8CTV3\quang <quanghm2010@gmail.com>
2023-05-06 00:46:29 +0700

    add GetCountByCompanyAgent

---
+++ Interfaces/Models/EmailInfo/EmailInfoCountByCARequest.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Interfaces.Model.Account
+{
+    public class EmailInfoCountByCARequest
+    {
+        public int assign { get; set; }
+        public int idCompany { get; set; }
+    }
+}
---
+++ Interfaces/Models/EmailInfo/EmailInfoCountByCAResponse.cs
@@ -0,0 +1,13 @@
+﻿using Interfaces.Base;
+using Interfaces.Constants;
+using System;
+using System.Collections.Generic;
+
+namespace Interfaces.Model.Account
+{
+    public class EmailInfoCountByCAResponse : BaseResponse<ResponseStatus>
+    {
+        public int All { get; set; }
+        public int ByAgent { get; set; }
+    }
+}
